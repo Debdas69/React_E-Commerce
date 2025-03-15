c797964de2a8c821b59ddbc07cbdc02ab68845d8
Debdas69 <deb05838@gmail.com>
2025-03-15 13:55:34 +0530

    filter product according to category

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,34 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bg from '../../assets/bg0.gif'
 import './Home.css'
 import { Catagory } from '../../Category'
 import Product from '../../components/Product/Product'
 
 const Home = () => {
+
+  let [cate, setCate] = useState(Catagory)
+  function filteredProducts(category) {
+    const updateData = Catagory.filter((item) => (item.category === category))
+    setCate(updateData)
+  }
   return (
-    <div className='home'>  
+    <div className='home'>
       <div className="hero-bg">
         <img src={bg} alt="" />
       </div>
       <div className="category-section">
-      {
-        Catagory.slice(0,5).map((product) => {
-          return (
-            <div key={product.id} className="category-card">
-              <img src={product.image} alt="" />
-              <span>{product.name}</span>             
+        {
+          Catagory.slice(0, 5).map((product) => {
+            return (
+              <div key={product.id} className="category-card" onClick={()=>{
+                filteredProducts(product.category)
+              }}>
+                <img src={product.image} alt="" />
+                <span>{product.name}</span>
               </div>
-          )
-        })
-      }
+            )
+          })
+        }
       </div>
       <h1>Trending Products</h1>
       <div className="product-section">
         {
-          Catagory.slice(0,7).map((product) => (
-            
-              <Product name={product.name} price={product.price} image={product.image} />
-            
+          cate.slice(0, 7).map((product) => (
+
+            <Product name={product.name} price={product.price} image={product.image} />
+
           ))
         }
       </div>
